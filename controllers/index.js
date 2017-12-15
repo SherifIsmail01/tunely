58e511058f7af9b8181488908b20308912a7a888
Sherif Ismail <sherifismail@proslamx.local>
2017-12-14 23:41:17 -0800

    sprint-0

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  api: require('./apiController'),
+  albums: require('./albumController')
+}
+
+module.exports = {
+  api: require('./albumController')
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+
+
+
+var express = require('express'),
+	bodyParser = require('body-parser');
+	db = require('./models');
+
+var controllers = require('./controllers');
+var app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.sendFile('views/index.html' , { root : __dirname});
+});
+
+app.get('/api', controllers.api.index);
+
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening at http://localhost:3000/');
+});
+
